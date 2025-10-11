037bbd298bf5040139ebbc13434271cc38681629
Takashi Masuda <masutaka.net@gmail.com>
2025-10-11 18:50:57 +0900

    Fix tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,7 +2,12 @@ require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @alice = users(:alice)
     @post = posts(:one)
+  end
+
+  def log_in_as(user)
+    post session_url, params: { email_address: user.email_address, password: "password" }
   end
 
   test "should get index" do
@@ -11,13 +16,15 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    log_in_as(@alice)
     get new_post_url
     assert_response :success
   end
 
   test "should create post" do
+    log_in_as(@alice)
     assert_difference("Post.count") do
-      post posts_url, params: { post: { body: @post.body, published_at: @post.published_at, slug: @post.slug, title: @post.title, user_id: @post.user_id } }
+      post posts_url, params: { post: { body: @post.body, published_at: @post.published_at, slug: @post.slug, title: @post.title } }
     end
 
     assert_redirected_to post_url(Post.last)
@@ -29,16 +36,19 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    log_in_as(@alice)
     get edit_post_url(@post)
     assert_response :success
   end
 
   test "should update post" do
-    patch post_url(@post), params: { post: { body: @post.body, published_at: @post.published_at, slug: @post.slug, title: @post.title, user_id: @post.user_id } }
+    log_in_as(@alice)
+    patch post_url(@post), params: { post: { body: @post.body, published_at: @post.published_at, slug: @post.slug, title: @post.title } }
     assert_redirected_to post_url(@post)
   end
 
   test "should destroy post" do
+    log_in_as(@alice)
     assert_difference("Post.count", -1) do
       delete post_url(@post)
     end
