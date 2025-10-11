db804aa91842cee616c27cc0ec04ee92449b719a
Takashi Masuda <masutaka.net@gmail.com>
2025-10-11 18:14:53 +0900

    Body を Markdown としてレンダリングする

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,27 @@
 module PostsHelper
+  def markdown(text)
+    return "" if text.blank?
+
+    options = {
+      filter_html: false,
+      hard_wrap: true,
+      link_attributes: { rel: "nofollow", target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
+
+    extensions = {
+      autolink: true,
+      superscript: true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks: true,
+      tables: true,
+      strikethrough: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
 end
