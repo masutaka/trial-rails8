1a458bd467464574fbe6fc9adeb7f7322f42612d
Takashi Masuda <masutaka.net@gmail.com>
2025-10-14 17:50:06 +0900

    Phase 1: Update db/migrate/20251014084856_add_published_to_posts.rb

--- db/migrate/20251014084856_add_published_to_posts.rb
+++ db/migrate/20251014084856_add_published_to_posts.rb
@@ -1,5 +1,8 @@
 class AddPublishedToPosts < ActiveRecord::Migration[8.0]
   def change
-    add_column :posts, :published, :boolean
+    add_column :posts, :published, :boolean, default: false, null: false
+    add_index :posts, :published_at
+    add_index :posts, :published
+    add_index :posts, [ :published, :published_at ]
   end
 end
