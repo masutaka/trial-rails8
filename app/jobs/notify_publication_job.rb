f4144e0abb23c58bda2641865f8648c12865b85b
Takashi Masuda <masutaka.net@gmail.com>
2025-10-17 19:00:31 +0900

    feat: (Phase 3) NotifyPublicationJobに通知作成ロジックを実装
    - 記事作成者以外の全ユーザーに未読通知を作成
    - find_eachを使用してメモリ効率的に処理
    - 記事が存在しない場合は早期リターン
    - 通知作成と例外ケースのテストを追加

--- app/jobs/notify_publication_job.rb
+++ app/jobs/notify_publication_job.rb
@@ -1,7 +1,13 @@
 class NotifyPublicationJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(post_id)
+    post = Post.find_by(id: post_id)
+    return unless post
+
+    # 記事の作成者以外の全ユーザーに通知を作成
+    User.where.not(id: post.user_id).find_each do |user|
+      Notification.create!(user: user, post: post, read: false)
+    end
   end
 end
--- test/jobs/notify_publication_job_test.rb
+++ test/jobs/notify_publication_job_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class NotifyPublicationJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "全ユーザー（作成者除く）に通知を作成する" do
+    post = posts(:ready_to_publish)
+    author = post.user
+    other_user = users(:alice) # ready_to_publish の作成者は bob なので alice は別ユーザー
+
+    assert_difference "Notification.count", 1 do
+      NotifyPublicationJob.perform_now(post.id)
+    end
+
+    # 作成者以外のユーザーに通知が作成されること
+    notification = Notification.find_by(post: post, user: other_user)
+    assert_not_nil notification
+    assert_equal post.id, notification.post_id
+    assert_equal other_user.id, notification.user_id
+    assert_equal false, notification.read
+
+    # 作成者には通知が作成されないこと
+    author_notification = Notification.find_by(post: post, user: author)
+    assert_nil author_notification
+  end
+
+  test "記事が見つからない場合は何もしない" do
+    invalid_post_id = 999999
+
+    assert_no_difference "Notification.count" do
+      assert_nothing_raised do
+        NotifyPublicationJob.perform_now(invalid_post_id)
+      end
+    end
+  end
 end
