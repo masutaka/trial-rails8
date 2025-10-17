d3623517a3c9ef02011cffea95a329e5e35bf98f
Takashi Masuda <masutaka.net@gmail.com>
2025-10-17 16:55:29 +0900

    Phase 1: Update db/migrate/20251017075303_create_notifications.rb

--- db/migrate/20251017075303_create_notifications.rb
+++ db/migrate/20251017075303_create_notifications.rb
@@ -1,11 +1,14 @@
 class CreateNotifications < ActiveRecord::Migration[8.0]
   def change
     create_table :notifications do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
-      t.boolean :read
+      t.references :user, null: false, foreign_key: { on_delete: :cascade }
+      t.references :post, null: false, foreign_key: { on_delete: :cascade }
+      t.boolean :read, default: false, null: false
 
       t.timestamps
     end
+
+    add_index :notifications, [ :user_id, :read, :created_at ]
+    add_index :notifications, [ :user_id, :created_at ]
   end
 end
