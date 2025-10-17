c59337097627ba0c82aa9bc13bd49725a82d097a
Takashi Masuda <masutaka.net@gmail.com>
2025-10-17 22:47:09 +0900

    feat: (Phase 4-2,3) 通知の既読機能を実装
    - NotificationsControllerにmark_as_readとmark_all_as_readアクションを追加
    - Userモデルにnotificationsの関連付けを追加
    - 認証チェックと権限チェックを含むコントローラテストを追加
    - 未読通知の一括既読機能を実装

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,12 @@
 class NotificationsController < ApplicationController
+  def mark_as_read
+    notification = Current.user.notifications.find_by(id: params[:id])
+    notification&.mark_as_read!
+    head :ok
+  end
+
+  def mark_all_as_read
+    Current.user.notifications.unread.each(&:mark_as_read!)
+    head :ok
+  end
 end
