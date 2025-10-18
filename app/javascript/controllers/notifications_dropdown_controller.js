d853998bed528c2c50a04f427ff237e6b70d5fa7
Takashi Masuda <masutaka.net@gmail.com>
2025-10-18 13:09:43 +0900

    feat: 通知ドロップダウンにEscキーでの閉じる機能を追加
    - Escキーでドロップダウンを閉じる handleEscape メソッドを追加
    - keydownイベントリスナーを connect/disconnect/open/close で適切に管理
    - requestAnimationFrameでイベントリスナー追加タイミングを最適化

--- app/javascript/controllers/notifications_dropdown_controller.js
+++ app/javascript/controllers/notifications_dropdown_controller.js
@@ -5,10 +5,12 @@ export default class extends Controller {
 
   connect() {
     this.boundHandleOutsideClick = this.handleOutsideClick.bind(this)
+    this.boundHandleEscape = this.handleEscape.bind(this)
   }
 
   disconnect() {
     document.removeEventListener("click", this.boundHandleOutsideClick)
+    document.removeEventListener("keydown", this.boundHandleEscape)
   }
 
   toggle(event) {
@@ -26,16 +28,24 @@ export default class extends Controller {
     // 次のフレームでイベントリスナーを追加（現在のクリックイベントが完了した後）
     requestAnimationFrame(() => {
       document.addEventListener("click", this.boundHandleOutsideClick)
+      document.addEventListener("keydown", this.boundHandleEscape)
     })
   }
 
   close() {
     this.dropdownTarget.classList.add("hidden")
     document.removeEventListener("click", this.boundHandleOutsideClick)
+    document.removeEventListener("keydown", this.boundHandleEscape)
   }
 
   handleOutsideClick(event) {
     if (!this.element.contains(event.target)) {
+      this.close()
+    }
+  }
+
+  handleEscape(event) {
+    if (event.key === "Escape") {
       this.close()
     }
   }
