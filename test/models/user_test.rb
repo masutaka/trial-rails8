85eca354a9aebe08b5d7294e63bc13d896e4d74a
Takashi Masuda <masutaka.net@gmail.com>
2025-10-30 13:43:56 +0900

    refactor: UserTest をネストクラスで機能ごとに整理
    - ValidationTest: username バリデーション関連のテスト
    - FollowTest: フォロー機能関連のテスト
    - comment_test.rb のパターンに合わせた構造に統一

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -17,135 +17,135 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # username バリデーションテスト
+  class ValidationTest < UserTest
+    test "username が存在しない場合、無効であること" do
+      user = User.new(email_address: "test@example.com", password: "password", username: nil)
+      assert_not user.valid?
+      assert_includes user.errors[:username], "can't be blank"
+    end
 
-  test "username が存在しない場合、無効であること" do
-    user = User.new(email_address: "test@example.com", password: "password", username: nil)
-    assert_not user.valid?
-    assert_includes user.errors[:username], "can't be blank"
-  end
+    test "username が空文字の場合、無効であること" do
+      user = User.new(email_address: "test@example.com", password: "password", username: "")
+      assert_not user.valid?
+      assert_includes user.errors[:username], "can't be blank"
+    end
 
-  test "username が空文字の場合、無効であること" do
-    user = User.new(email_address: "test@example.com", password: "password", username: "")
-    assert_not user.valid?
-    assert_includes user.errors[:username], "can't be blank"
-  end
+    test "username が3文字未満の場合、無効であること" do
+      user = User.new(email_address: "test@example.com", password: "password", username: "ab")
+      assert_not user.valid?
+      assert_includes user.errors[:username], "is too short (minimum is 3 characters)"
+    end
 
-  test "username が3文字未満の場合、無効であること" do
-    user = User.new(email_address: "test@example.com", password: "password", username: "ab")
-    assert_not user.valid?
-    assert_includes user.errors[:username], "is too short (minimum is 3 characters)"
-  end
+    test "username が30文字を超える場合、無効であること" do
+      user = User.new(email_address: "test@example.com", password: "password", username: "a" * 31)
+      assert_not user.valid?
+      assert_includes user.errors[:username], "is too long (maximum is 30 characters)"
+    end
 
-  test "username が30文字を超える場合、無効であること" do
-    user = User.new(email_address: "test@example.com", password: "password", username: "a" * 31)
-    assert_not user.valid?
-    assert_includes user.errors[:username], "is too long (maximum is 30 characters)"
-  end
+    test "username が小文字英数字、ハイフン、アンダースコアのみの場合、有効であること" do
+      valid_usernames = [ "abc", "test_user", "user-name", "user123", "a1_b2-c3" ]
+      valid_usernames.each do |username|
+        user = User.new(email_address: "test@example.com", password: "password", username: username)
+        assert user.valid?, "#{username} は有効であるべきですが、無効と判定されました"
+      end
+    end
 
-  test "username が小文字英数字、ハイフン、アンダースコアのみの場合、有効であること" do
-    valid_usernames = [ "abc", "test_user", "user-name", "user123", "a1_b2-c3" ]
-    valid_usernames.each do |username|
-      user = User.new(email_address: "test@example.com", password: "password", username: username)
-      assert user.valid?, "#{username} は有効であるべきですが、無効と判定されました"
+    test "username に大文字が含まれる場合、小文字に正規化されること" do
+      user = User.new(email_address: "test@example.com", password: "password", username: "TestUser")
+      assert user.valid?
+      assert_equal "testuser", user.username
+    end
+
+    test "username に記号（ハイフン、アンダースコア以外）が含まれる場合、無効であること" do
+      invalid_usernames = [ "test@user", "test.user", "test user", "test!user" ]
+      invalid_usernames.each do |username|
+        user = User.new(email_address: "test@example.com", password: "password", username: username)
+        assert_not user.valid?, "#{username} は無効であるべきですが、有効と判定されました"
+        assert_includes user.errors[:username], "is invalid"
+      end
+    end
+
+    test "username が重複する場合、無効であること" do
+      User.create!(email_address: "user1@example.com", password: "password", username: "testuser")
+      user = User.new(email_address: "user2@example.com", password: "password", username: "testuser")
+      assert_not user.valid?
+      assert_includes user.errors[:username], "has already been taken"
+    end
+
+    test "username が大文字小文字のみ異なる場合、無効であること（一意性の大文字小文字を区別しない）" do
+      User.create!(email_address: "user1@example.com", password: "password", username: "testuser")
+      user = User.new(email_address: "user2@example.com", password: "password", username: "TestUser")
+      assert_not user.valid?
+      assert_includes user.errors[:username], "has already been taken"
+    end
+
+    test "username が小文字に正規化されること" do
+      user = User.new(email_address: "test@example.com", password: "password", username: "TestUser")
+      user.valid?
+      assert_equal "testuser", user.username
     end
   end
 
-  test "username に大文字が含まれる場合、小文字に正規化されること" do
-    user = User.new(email_address: "test@example.com", password: "password", username: "TestUser")
-    assert user.valid?
-    assert_equal "testuser", user.username
-  end
+  class FollowTest < UserTest
+    test "following 関連付けが正しく動作すること" do
+      user1 = User.create!(email_address: "user1@example.com", password: "password", username: "user1")
+      user2 = User.create!(email_address: "user2@example.com", password: "password", username: "user2")
+      user3 = User.create!(email_address: "user3@example.com", password: "password", username: "user3")
 
-  test "username に記号（ハイフン、アンダースコア以外）が含まれる場合、無効であること" do
-    invalid_usernames = [ "test@user", "test.user", "test user", "test!user" ]
-    invalid_usernames.each do |username|
-      user = User.new(email_address: "test@example.com", password: "password", username: username)
-      assert_not user.valid?, "#{username} は無効であるべきですが、有効と判定されました"
-      assert_includes user.errors[:username], "is invalid"
+      Follow.create!(follower: user1, followed: user2)
+      Follow.create!(follower: user1, followed: user3)
+
+      assert_equal 2, user1.following.count
+      assert_includes user1.following, user2
+      assert_includes user1.following, user3
     end
-  end
 
-  test "username が重複する場合、無効であること" do
-    User.create!(email_address: "user1@example.com", password: "password", username: "testuser")
-    user = User.new(email_address: "user2@example.com", password: "password", username: "testuser")
-    assert_not user.valid?
-    assert_includes user.errors[:username], "has already been taken"
-  end
+    test "followers 関連付けが正しく動作すること" do
+      user1 = User.create!(email_address: "user1@example.com", password: "password", username: "user1")
+      user2 = User.create!(email_address: "user2@example.com", password: "password", username: "user2")
+      user3 = User.create!(email_address: "user3@example.com", password: "password", username: "user3")
 
-  test "username が大文字小文字のみ異なる場合、無効であること（一意性の大文字小文字を区別しない）" do
-    User.create!(email_address: "user1@example.com", password: "password", username: "testuser")
-    user = User.new(email_address: "user2@example.com", password: "password", username: "TestUser")
-    assert_not user.valid?
-    assert_includes user.errors[:username], "has already been taken"
-  end
+      Follow.create!(follower: user2, followed: user1)
+      Follow.create!(follower: user3, followed: user1)
 
-  test "username が小文字に正規化されること" do
-    user = User.new(email_address: "test@example.com", password: "password", username: "TestUser")
-    user.valid?
-    assert_equal "testuser", user.username
-  end
+      assert_equal 2, user1.followers.count
+      assert_includes user1.followers, user2
+      assert_includes user1.followers, user3
+    end
 
-  # フォロー機能テスト
+    test "follow メソッドでユーザーをフォローできること" do
+      user1 = User.create!(email_address: "user1@example.com", password: "password", username: "user1")
+      user2 = User.create!(email_address: "user2@example.com", password: "password", username: "user2")
 
-  test "following 関連付けが正しく動作すること" do
-    user1 = User.create!(email_address: "user1@example.com", password: "password", username: "user1")
-    user2 = User.create!(email_address: "user2@example.com", password: "password", username: "user2")
-    user3 = User.create!(email_address: "user3@example.com", password: "password", username: "user3")
+      assert_difference "Follow.count", 1 do
+        user1.follow(user2)
+      end
 
-    Follow.create!(follower: user1, followed: user2)
-    Follow.create!(follower: user1, followed: user3)
+      assert_includes user1.following, user2
+    end
 
-    assert_equal 2, user1.following.count
-    assert_includes user1.following, user2
-    assert_includes user1.following, user3
-  end
+    test "unfollow メソッドでフォローを解除できること" do
+      user1 = User.create!(email_address: "user1@example.com", password: "password", username: "user1")
+      user2 = User.create!(email_address: "user2@example.com", password: "password", username: "user2")
 
-  test "followers 関連付けが正しく動作すること" do
-    user1 = User.create!(email_address: "user1@example.com", password: "password", username: "user1")
-    user2 = User.create!(email_address: "user2@example.com", password: "password", username: "user2")
-    user3 = User.create!(email_address: "user3@example.com", password: "password", username: "user3")
-
-    Follow.create!(follower: user2, followed: user1)
-    Follow.create!(follower: user3, followed: user1)
-
-    assert_equal 2, user1.followers.count
-    assert_includes user1.followers, user2
-    assert_includes user1.followers, user3
-  end
-
-  test "follow メソッドでユーザーをフォローできること" do
-    user1 = User.create!(email_address: "user1@example.com", password: "password", username: "user1")
-    user2 = User.create!(email_address: "user2@example.com", password: "password", username: "user2")
-
-    assert_difference "Follow.count", 1 do
       user1.follow(user2)
+
+      assert_difference "Follow.count", -1 do
+        user1.unfollow(user2)
+      end
+
+      assert_not_includes user1.following, user2
     end
 
-    assert_includes user1.following, user2
-  end
+    test "following? メソッドでフォロー状態を確認できること" do
+      user1 = User.create!(email_address: "user1@example.com", password: "password", username: "user1")
+      user2 = User.create!(email_address: "user2@example.com", password: "password", username: "user2")
+      user3 = User.create!(email_address: "user3@example.com", password: "password", username: "user3")
 
-  test "unfollow メソッドでフォローを解除できること" do
-    user1 = User.create!(email_address: "user1@example.com", password: "password", username: "user1")
-    user2 = User.create!(email_address: "user2@example.com", password: "password", username: "user2")
+      user1.follow(user2)
 
-    user1.follow(user2)
-
-    assert_difference "Follow.count", -1 do
-      user1.unfollow(user2)
+      assert user1.following?(user2)
+      assert_not user1.following?(user3)
     end
-
-    assert_not_includes user1.following, user2
-  end
-
-  test "following? メソッドでフォロー状態を確認できること" do
-    user1 = User.create!(email_address: "user1@example.com", password: "password", username: "user1")
-    user2 = User.create!(email_address: "user2@example.com", password: "password", username: "user2")
-    user3 = User.create!(email_address: "user3@example.com", password: "password", username: "user3")
-
-    user1.follow(user2)
-
-    assert user1.following?(user2)
-    assert_not user1.following?(user3)
   end
 end
